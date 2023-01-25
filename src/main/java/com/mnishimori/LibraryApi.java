261c793cdf5017eaa0e6280658de34fcf04dc409
mnishimori <mnishimori@yahoo.com.br>
2023-01-25 13:53:00 -0300

    Primeiro commit do projeto

---
+++ src/main/java/com/mnishimori/LibraryApi.java
@@ -0,0 +1,11 @@
+package com.mnishimori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApi {
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApi.class, args);
+    }
+}
